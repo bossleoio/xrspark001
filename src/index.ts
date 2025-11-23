0f939a0c80f800b3a7f6353810e55237593ce4d5
Marlaina Love <58565525+mlove93@users.noreply.github.com>
2025-11-23 15:03:27 -0600

    Initial commit: freeze current codebase state

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import {
+  AssetManifest,
+  AssetType,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  SessionMode,
+  SRGBColorSpace,
+  AssetManager,
+  World,
+} from "@iwsdk/core";
+
+import { PanelSystem } from "./panel.js";
+
+import { RobotSystem } from "./robot.js";
+
+const assets: AssetManifest = {
+  chimeSound: {
+    url: "/audio/chime.mp3",
+    type: AssetType.Audio,
+    priority: "background",
+  },
+  webxr: {
+    url: "/textures/webxr.png",
+    type: AssetType.Texture,
+    priority: "critical",
+  },
+};
+
+World.create(document.getElementById("scene-container") as HTMLDivElement, {
+  assets,
+  xr: {
+    sessionMode: SessionMode.ImmersiveAR,
+    offer: "always",
+    // Optional structured features; layers/local-floor are offered by default
+    features: {
+      handTracking: true,
+      anchors: true,
+      hitTest: true,
+      planeDetection: true,
+      meshDetection: true,
+      layers: true,
+    },
+  },
+  features: {
+    locomotion: false,
+    grabbing: true,
+    physics: false,
+    sceneUnderstanding: true,
+  },
+  level: "/glxf/Composition.glxf",
+}).then((world) => {
+  const { camera } = world;
+
+  camera.position.set(0, 1, 0.5);
+
+  const webxrLogoTexture = AssetManager.getTexture("webxr")!;
+  webxrLogoTexture.colorSpace = SRGBColorSpace;
+  const logoBanner = new Mesh(
+    new PlaneGeometry(3.39, 0.96),
+    new MeshBasicMaterial({
+      map: webxrLogoTexture,
+      transparent: true,
+    }),
+  );
+  world.createTransformEntity(logoBanner);
+  logoBanner.position.set(0, 1, 1.8);
+  logoBanner.rotateY(Math.PI);
+
+  world.registerSystem(PanelSystem).registerSystem(RobotSystem);
+});
